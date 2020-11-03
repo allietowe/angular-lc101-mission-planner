c1a97906d16ec60ab8896bd2e9876add0e562b36
Allie Towe <towe.allie@yahoo.com>
2020-11-02 20:13:31 -0600

    finished

--- mission-planning-dashboard/src/app/header/header.component.ts
+++ mission-planning-dashboard/src/app/header/header.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
   missionName: string = "Mars 2030";
   rocketName: string = "Plasma Max";
+  nasaPng: string = "https://www.pngfind.com/pngs/m/95-958022_nasa-sticker-hd-png-download.png";
   constructor() { }
 
   ngOnInit() {
